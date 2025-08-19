0df8e6c74203eb9d7bba5b391a026ee4ee885be4
Juan Esteban Cárdenas Cardona <juanesteban1417@gmail.com>
2025-08-18 22:33:17 -0500

    Complete exercise 01

---
+++ LogicConcepts/isNumberEvenOrNot/Program.cs
@@ -0,0 +1,33 @@
+﻿
+var numberString = string.Empty;
+
+do
+{
+    Console.Write("Ingrese un número entero o la palabra 'salir' para terminar el programa: ");
+    numberString = Console.ReadLine();
+
+    if (numberString!.ToLower() == "salir")
+    {
+        continue;
+    }
+
+    int number = 0;
+    if (int.TryParse(numberString, out number))
+    {
+        if (number % 2 == 0)
+        {
+            Console.WriteLine($"El número {number}, es entero.");
+        }
+        else
+        {
+            Console.WriteLine($"El número {number}, es impar.");
+        }
+    }else
+    {
+        Console.WriteLine($"El valor ingresado: '{numberString}', no es un número entero.");
+    }
+
+    Console.WriteLine(string.Empty);
+
+} while (numberString!.ToLower() != "salir");
+Console.WriteLine("Game Over!");
